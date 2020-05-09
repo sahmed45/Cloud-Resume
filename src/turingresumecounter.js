6520d86ac18677d0d295dc747d2fbe9a21f7188f
Sean Ziegler <seanziegler@Seans-MacBook-Pro.local>
2020-05-09 11:13:40 -0400

    JS error

--- src/turingresumecounter.js
+++ src/turingresumecounter.js
@@ -4,7 +4,7 @@ var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
        // Typical action to be performed when the document is ready:
-       document.getElementById("visitors").innerHTML = xhttp.responseText;
+       document.getElementById("visits").innerHTML = xhttp.responseText;
     }
 };
 xhttp.open("GET", "https://bwc2pl2iz5.execute-api.us-east-1.amazonaws.com/Prod/counter", true);
