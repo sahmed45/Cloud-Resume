af56c95be9f537f0fdac57228329033aa066840d
Sean Ziegler <seanziegler@Seans-MacBook-Pro.local>
2020-06-16 17:45:15 -0400

    Formatting with prettier

--- lambda/function.py
+++ lambda/function.py
@@ -1,21 +1,21 @@
-import boto3
 import json
+import boto3
+
 
 def handler(event, context):
-    '''When this function is invoked - retrive visitor count, add one, store in database, return to client '''
+    '''When this function is invoked - retrive visitor count, add one, store
+    in database, return to client '''
     dynamodb = boto3.client('dynamodb')
-    
-    #Get Visits
-    response = dynamodb.get_item(TableName='turingresumecounter', Key={'Site':{'N': '0'}})
-    
+
+    # Get Visits
+    response = dynamodb.get_item(
+        TableName='turingresumecounter', Key={'Site': {'N': '0'}})
+
     visits = int(response["Item"]["Visits"]["N"]) + 1
 
-    visits = ['baar','baz']
-
-    visits.l
-
-    #Store Visits
-    dynamodb.put_item(TableName='turingresumecounter', Item={'Site':{'N': '0'},'Visits':{'N': str(visits)}})
-    
+    # Store Visits
+    dynamodb.put_item(TableName='turingresumecounter', Item={
+        'Site': {'N': '0'},
+        'Visits': {'N': str(visits)}
+    })
     return visits
-
